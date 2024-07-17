0d39190ecb624c738161643df7754f26e50245b1
TamasGaramvolgyi <tamas.garamvoelgyi@burk-consulting.com>
2024-07-17 13:29:08 +0200

    Reapply "testVault"
    This reverts commit 6b5ee1d25a6f59d115dc7b56435dddffa61ce120.

--- BurkWebAPI/Program.cs
+++ BurkWebAPI/Program.cs
@@ -1,7 +1,10 @@
-using BurkWebAPI.Data;
+using Azure.Identity;
 using BurkWebAPI.Interfaces;
 using BurkWebAPI.Repository;
+using Azure.Security.KeyVault.Secrets;
+using BurkWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,43 @@ builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(options =>
+if (builder.Environment.IsProduction())
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    var keyVaultURI = new Uri($"{builder.Configuration["KeyVault:KeyVaultURL"]}");
+    var credential = new DefaultAzureCredential();
+
+    builder.Configuration.AddAzureKeyVault(keyVaultURI, credential, new KeyVaultSecretManager());
+    var client = new SecretClient(keyVaultURI, credential);
+
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(client.GetSecret("AzureConnection").Value.Value.ToString());
+    });
+}
+
+//if (builder.Environment.IsDevelopment())
+//{
+//    var keyVaultURL = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
+
+//    var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+
+//    builder.Configuration.AddAzureKeyVault(keyVaultURL.Value!.ToString(), new DefaultKeyVaultSecretManager());
+
+//    var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), new DefaultAzureCredential());
+
+//    builder.Services.AddDbContext<DataContext>(options =>
+//    {
+//        options.UseSqlServer(client.GetSecret("AzureConnection").Value.Value.ToString());
+//    });
+//}
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+}
 
 var app = builder.Build();
 
